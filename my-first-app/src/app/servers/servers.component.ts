5250a71d0dd2de8a7ff526ce74b80e8ad54e3f28
Matías Dell'Oso <matidelloso@gmail.com>
2017-09-07 12:11:11 -0300

    Propertty Binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ServersComponent implements OnInit {
+  
+  allowNewServer: boolean = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);  
+   }
 
   ngOnInit() {
   }
